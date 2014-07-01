a5769e821b179a427d7e3953f7e25b52a53a42bc
Vladimir Turnaev <turnaev@gmail.com>
2014-07-01 13:35:31 +0700

    ~

--- catalogs.php
+++ catalogs.php
@@ -1,4 +1,6 @@
 ﻿<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+
+<!---->
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
 <head>
     <meta http-equiv="content-type" content="text/html; charset=utf-8" />
